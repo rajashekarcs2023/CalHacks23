1670e00ee677ac1d2f0e8458453ab19fa30b0a5c
rajashekarcs2023 <114786116+rajashekarcs2023@users.noreply.github.com>
2023-11-01 14:24:34 -0700

    commit2

---
+++ front-end/public/index.ts
@@ -0,0 +1,67 @@
+import logo from './logo.png'
+import dashboard from './dashboard.svg'
+import health from './health.svg'
+import blood from './blood.svg'
+import profile from './profile.svg'
+import upload from './upload.svg'
+import dropdown from './dropdown.svg'
+import menu from './menu.svg'
+import inhale from './inhale.svg'
+import blood_blue from './blood_blue.svg'
+import blood_chart from './blood_chart.svg'
+import blood_sugar from './blood_sugar.svg'
+import heart_chart from './heart_chart.svg'
+import heart from './heart.svg'
+import sugar_chart from './sugar_chart.svg'
+import arrow_down from './arrow_down.svg'
+import search from './search.svg'
+import ring from './ring.svg'
+import down_arrow from './down_arrow.svg'
+import up_arrow from './up_arrow.svg'
+import man from './man.png'
+import fat from './fat.svg'
+import stress from './stress.svg'
+import heart1 from './heart1.svg'
+import hour from './hour.svg'
+import triglycerides_chart from './triglycerides_chart.svg'
+import heart_attack_chart from './heart_attack_chart.svg'
+import hour_chart from './hour_chart.svg'
+import stress_chart from './stress_chart.svg'
+import chat from './documentist.svg'
+import send from './send.svg'
+import chat1 from './chat.svg'
+
+export {
+  logo,
+  dashboard,
+  health,
+  blood,
+  profile,
+  upload,
+  dropdown,
+  inhale,
+  menu,
+  blood_blue,
+  blood_chart,
+  blood_sugar,
+  heart_chart,
+  heart,
+  sugar_chart,
+  arrow_down,
+  search,
+  ring,
+  down_arrow,
+  up_arrow,
+  man,
+  fat,
+  stress,
+  heart1,
+  hour,
+  triglycerides_chart,
+  heart_attack_chart,
+  hour_chart,
+  stress_chart,
+  send,
+  chat,
+  chat1,
+}
